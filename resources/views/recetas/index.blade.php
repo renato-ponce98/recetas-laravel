26e838776f9f802bd36354dd4d9c1ead7a8e7dfb
Renato Ponce <69768980+renato-ponce98@users.noreply.github.com>
2020-08-17 12:55:56 -0500

    Primer Commit
    Se llevo a cabo el primer commit del proyecto

---
+++ resources/views/recetas/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Recetas</h1>
+
+@foreach($recetas as $receta)
+<li>{{$receta}}</li>
+@endforeach
+
+<h1>Categorias</h1>
+
+@foreach($categorias as $categoria)
+<li>{{$categoria}}</li>
+@endforeach
+@endsection
+
